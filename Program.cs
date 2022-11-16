9e64f582f19db6d1330018c6b6ce1cc2c783562c
Jacob Thomas <jacgretho@gmail.com>
2022-11-16 14:27:27 -0600

    removed initial source code and added welcome message

--- Program.cs
+++ Program.cs
@@ -1,12 +1,3 @@
 ﻿using System;
 
-namespace EnthusiasticMoose
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
